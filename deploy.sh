c71d52a8cb3bc7b1ea48cf63654947356fc5dc42
John Zhang <johnz@mit.edu>
2017-12-24 21:41:53 +0000

    Switched back to kiss.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
-hugo -t ghostwriter
+hugo -t kiss
 # Go To Public folder
 cd public/
 # Add changes to git.
