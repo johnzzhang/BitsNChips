e603ae560857232323268ffe7212daa208d30ad8
John Zhang <johnz@mit.edu>
2017-12-24 21:37:59 +0000

    Switched themes.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
-hugo -t kiss
+hugo -t ghostwriter
 # Go To Public folder
 cd public/
 # Add changes to git.
